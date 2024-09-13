e2428458cdb461bb6334c9b64a6f2a1f71d239eb
Rohit Mahuli <128014013+rohitmuta@users.noreply.github.com>
2024-09-13 15:50:04 -0500

    Add files via upload

---
+++ GenericRepository.cs
@@ -0,0 +1,101 @@
+﻿using Hospital.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories.Implementation
+{
+    public class GenericRepository<T> : IDisposable, IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+            _context.SaveChanges();  // Save changes to persist the addition
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();  // Save changes to persist the addition
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            _context.SaveChanges();  // Save changes to persist the deletion
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            dbSet.Remove(entity);
+            await _context.SaveChangesAsync();  // Save changes to persist the deletion
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();  // Dispose of the DbContext
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return query.AsEnumerable();
+        }
+
+        public T GetById(object id)
+        {
+            return dbSet.Find(id);  // Find entity by primary key
+        }
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            return await dbSet.FindAsync(id);  // Find entity by primary key asynchronously
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();  // Save changes to persist the update
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();  // Save changes to persist the update asynchronously
+        }
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Hospital.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        public IGenericRepository<T> GenericRepository<T>() where T : class
+        {
+            if (_repositories.ContainsKey(typeof(T)))
+            {
+                return (IGenericRepository<T>)_repositories[typeof(T)];
+            }
+
+            var repositoryType = typeof(GenericRepository<T>);
+            var repositoryInstance = (IGenericRepository<T>)Activator.CreateInstance(repositoryType, _context);
+            _repositories.Add(typeof(T), repositoryInstance);
+
+            return repositoryInstance;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
